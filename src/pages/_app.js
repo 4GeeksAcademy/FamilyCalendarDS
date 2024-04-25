7f0d5c4d596c9bf3a0d0f2ddea00f8018dc2387d
DanielSantiago2024 <157153010+DanielSantiago2024@users.noreply.github.com>
2024-04-25 00:35:56 +0000

    React Query

---
+++ src/pages/_app.js
@@ -0,0 +1,22 @@
+import "@/styles/globals.css";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+const client = new QueryClient();
+
+export default function MyApp({ Component, pageProps}) {
+    const [supabaseClient] = useState(() => createPagesBrowserClient());
+
+    return (
+        <QueryClientProvider client={client}>
+        <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+        >
+         <Component {...pageProps} />
+        </SessionContextProvider>
+        </QueryClientProvider>
+    );
+}
